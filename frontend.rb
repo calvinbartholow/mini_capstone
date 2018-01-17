b4dcc897c3679148541f526bf28e3087a2ff222a
Calvin Bartholow <calvinbartholow@gmail.com>
2018-01-16 20:33:40 -0800

    added frontend.rb

--- frontend.rb
+++ frontend.rb
@@ -6,6 +6,12 @@ puts "[2] show all products"
 
 user_input = gets.chomp 
 
-if user_input == 1 
-  response = Unirest.get()
+if user_input == "1" 
+  response = Unirest.get("http://localhost:3000/single")
+  product = response.body 
+  puts JSON.pretty_generate(product)
+elsif user_input == "2"
+  response = Unirest.get("http://localhost:3000/all") 
+  products = response.body 
+  puts JSON.pretty_generate(products)
 end 
