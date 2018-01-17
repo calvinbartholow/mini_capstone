b33136e45b3042b2e79848c37c862169cafa8a29
Calvin Bartholow <calvinbartholow@gmail.com>
2018-01-16 19:46:48 -0800

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+guayaki = Product.new(title:"Guayaki", price:"1.99", image_url:"https://cdn-a.william-reed.com/var/wrbm_gb_food_pharma/storage/images/2/5/8/0/40852-3-eng-GB/New-Cans-Guayaki.jpg", description:"organic energy")
+guayaki.save 
+
+fried_chicken = Product.new(title:"chicken", price:"5.00", image_url:"https://i.ytimg.com/vi/mTGuRxX3ZTo/maxresdefault.jpg", description:"finger lickin good")
+fried_chicken.save
+
