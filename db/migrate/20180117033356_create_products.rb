359956eaa7aaa7a433927db876f71e1ea8145f75
Calvin Bartholow <calvinbartholow@gmail.com>
2018-01-16 19:35:49 -0800

    new model w/attributes

--- db/migrate/20180117033356_create_products.rb
+++ db/migrate/20180117033356_create_products.rb
@@ -1,6 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
+      t.string :title
+      t.string :price
+      t.string :image_url
+      t.string :description
 
       t.timestamps
     end
